fcc765bc2899f5852a11d993c206c69036755289
Sujoy chakraborty <sujoyc070@gmail.com>
2023-06-09 00:28:27 +0600

    review done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 // active class//
 $(".chooseItem").click(function () {
   $(".chooseItem").removeClass("active");
-  // $(".tab").addClass("active"); // instead of this do the below 
   $(this).addClass("active");   
 });
+
 // Slider script//
 $('.video-slider').slick({
     dots: false,
     infinite: true,
+    autoplay: true,
     prevArrow: false,
     nextArrow: '<span class="next"><i class="fa-solid fa-angle-right"></i></span>',
     speed: 300,
     slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+// Review Slider script//
+$('.review-slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    prevArrow: false,
+    nextArrow: '<span class="next"><i class="fa-solid fa-angle-right"></i></span>',
+    speed: 300,
+    slidesToShow: 1,
     slidesToScroll: 1,
   });
