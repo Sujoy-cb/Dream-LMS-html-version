85cc38967a5519a81ae8680c19d63b29d0f985d9
Sujoy chakraborty <sujoyc070@gmail.com>
2023-06-25 23:40:19 +0600

    profile setting 20%

--- js/profilesetting.js
+++ js/profilesetting.js
@@ -1,5 +1,44 @@
-// Profile Setting//
-$(".tabs").click(function () {
-    $(".tabs").removeClass("active");
-    $(this).addClass("active");   
-  });
+let abc = document.querySelector(".abc")
+let profilepicture = document.querySelector(".profilepicture")
+let privacysetting = document.querySelector(".privacysetting")
+let myList = document.querySelector(".my-list")
+let active = document.querySelector(".active")
+let udfile = document.querySelector(".up")
+let propic = document.querySelector(".pp")
+let proset = document.querySelector(".ps")
+let a = document.querySelector(".a")
+let b = document.querySelector(".b")
+let c = document.querySelector(".c")
+
+let udemyProf = () => {
+  if (active.classList.contains("active")) {
+    udfile.classList.add("active")
+    propic.classList.remove("active")
+    proset.classList.remove("active")
+    a.classList.add("abc")
+    b.classList.remove("profilepicture")
+    c.classList.remove("privacysetting")
+  }
+}
+
+let profic = () => {
+  if (active.classList.contains("active")) {
+    propic.classList.add("active")
+    proset.classList.remove("active")
+    udfile.classList.remove("active")
+    b.classList.add("profilepicture")
+    a.classList.remove("abc")
+    c.classList.remove("privacysetting")
+  }
+}
+
+let privSetting = () => {
+  if (active.classList.contains("active")) {
+    proset.classList.add("active")
+    propic.classList.remove("active")
+    udfile.classList.remove("active")
+    c.classList.add("privacysetting")
+    b.classList.remove("profilepicture")
+    a.classList.remove("abc")
+  }
+}
