1554554b91e3b0e4bacb7594973e94bbb9dcb686
Sujoy chakraborty <sujoyc070@gmail.com>
2023-07-05 01:25:35 +0600

    video part

---
+++ js/videoplayer.js
@@ -0,0 +1,11 @@
+// Video Player Jquery //
+
+// As a jQuery plugin
+$('video, audio').mediaelementplayer({
+    // more configuration here
+  });
+  
+  // As a Vanilla JavaScript plugin
+  var player = new MediaElementPlayer('player', {
+      // more configurations here
+  });
