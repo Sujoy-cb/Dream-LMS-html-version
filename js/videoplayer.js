a46ae9d79c1c75e51069e80a77ff50b3c83e24c2
Sujoy chakraborty <sujoyc070@gmail.com>
2023-07-05 16:54:24 +0600

    video section

--- js/videoplayer.js
+++ js/videoplayer.js
@@ -1,11 +1,35 @@
 // Video Player Jquery //
 
-// As a jQuery plugin
-$('video, audio').mediaelementplayer({
-    // more configuration here
-  });
-  
-  // As a Vanilla JavaScript plugin
-  var player = new MediaElementPlayer('player', {
-      // more configurations here
-  });
+const player = new Plyr('video', {captions: {active: true}});
+
+// Expose player so it can be used from the console
+window.player = player;
+
+
+
+
+// Video description functions //
+
+let ov = document.querySelector(".overview")
+let active = document.querySelector(".active")
+
+
+let videoinfo = document.querySelector(".info")
+let videoreview = document.querySelector(".review")
+
+
+let videoInfo = ()=>{
+    if(active.classList.contains("active")){
+        videoinfo.classList.add("active")
+        videoreview.classList.remove("active")
+    }
+}
+
+let videoReivew = ()=>{
+    if(active.classList.contains("active")){
+        videoreview.classList.add("active")
+        videoinfo.classList.remove("active")
+    }
+}
+
+
